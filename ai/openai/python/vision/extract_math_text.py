7d0de849e201e1d15f030bd9298bfa6eac0de145
Drazen Zadravec <drazenzadravec@users.noreply.github.com>
2025-05-09 12:39:27 +1000

    Create extract_math_text.py

---
+++ ai/openai/python/vision/extract_math_text.py
@@ -0,0 +1,52 @@
+
+import openai
+import base64
+
+# supply your API key however you choose
+openai.api_key = "OPENAI-API-KEY"
+
+system_prompt = (
+    "The user will provide you an image of a document file. Perform the following actions: "
+    "1. extract the text in the document."
+    "2. extract each math equation."
+)
+
+# Function to encode the image
+def encode_image(local_image_path):
+    with open(local_image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+# Pass the image to the LLM for interpretation  
+def get_vision_response(prompt, image_path):
+
+    # Getting the base64 string
+    base64_image = encode_image(image_path)
+
+    response = openai.ChatCompletion.create(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "system",
+                "content": system_prompt
+            },
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", 
+                        "text": prompt},
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{base64_image}"
+                        },
+                    },
+                ],
+            }
+        ],
+    )
+    return response
+
+
+response = get_vision_response("image contains text and math equations", "PATH-TO-YOUR-IMAGE")
+print(response['choices'][0]['message']['content'])
+print(response)
