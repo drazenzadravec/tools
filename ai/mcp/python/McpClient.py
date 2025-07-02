ddfeeb86c4593f7de322ccf1380d75041fae83f4
Drazen Zadravec <drazenzadravec@users.noreply.github.com>
2025-07-02 12:52:36 +1000

    added MCP tools

---
+++ ai/mcp/python/McpClient.py
@@ -0,0 +1,307 @@
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+from mcp.client.streamable_http import streamablehttp_client
+
+from typing import Optional, Any, List, Union
+from contextlib import AsyncExitStack
+
+from .McpServerBase import McpTool, McpPrompt, McpResource
+
+# Model context protocol client.
+class McpClient:
+    """
+    Model context protocol client.
+    """
+    def __init__(self):
+        self.open = False
+
+        # Initialize session and client objects
+        self.session: Optional[ClientSession] = None
+        self.httpSession: ClientSession = None
+        self.exit_stack = AsyncExitStack()
+
+        # init
+        self.tools: List[McpTool] = [];
+        self.prompts: List[McpPrompt] = [];
+        self.resources: List[McpResource] = [];
+
+    def isConnected(self) -> bool:
+        """
+        is connected to MCP server.
+
+        Return:
+            true if connected to server; else false.
+        """
+        return self.open
+
+    def getTools(self) -> List[McpTool]:
+        """
+        get the list of tools
+
+        Return:
+            the list of tools; else empty.
+        """
+        return self.tools
+
+    def getPrompts(self) -> List[McpPrompt]:
+        """
+        get the list of prompts
+
+        Return:
+            the list of prompts; else empty.
+        """
+        return self.prompts
+
+    def getResources(self) -> List[McpResource]:
+        """
+        get the list of resources
+
+        Return:
+            the list of resources; else empty.
+        """
+        return self.resources
+
+    async def callTool(self, name: str, args: dict[str, Any] | None = None) -> Union[Any, None]:
+        """
+        call the tool.
+
+        Args:
+            name:    the name of the tool
+            args:    the arguments
+
+        Return:
+            the result; else none.
+        """
+        # if open.
+        if self.open:
+            return await self.session.call_tool(name, arguments = args)
+        else:
+            return None
+
+    async def callPrompt(self, name: str, args: Any) -> Union[Any, None]:
+        """
+        read the resource.
+
+        Args:
+            name:    the name of the prompt
+            args:    the arguments
+
+        Return:
+            the result; else none.
+        """
+        # if open.
+        if self.open:
+            return await self.session.get_prompt(name, arguments = args)
+        else:
+            return None
+
+    async def callResource(self, uri: str) -> Union[Any, None]:
+        """
+        read the resource.
+
+        Args:
+            uri:    the resource URI
+
+        Return:
+            the result; else none.
+        """
+        # if open.
+        if self.open:
+            return await self.session.read_resource(uri)
+        else:
+            return None
+
+    async def closeConnection(self):
+        """
+        disconnect from the MCP server.
+        """
+        # if open.
+        if self.open:
+            self.session = None
+            self.httpSession = None
+            await self.exit_stack.aclose()
+            self.open = False
+
+            self.tools = [];
+            self.prompts = [];
+            self.resources = [];
+
+    async def openConnectionStdio(self, serverScriptPath: str):
+        """
+        connect to the MCP server.
+        start receiving messages on stdin and sending messages on stdout.
+
+        Args:
+            serverScriptPath: the server script full path
+        """
+
+        # if not open.
+        if not self.open:
+            try:
+
+                # only if JavaScript or Python
+                is_python = serverScriptPath.endswith('.py')
+                is_js = serverScriptPath.endswith('.js')
+
+                # if not JavaScript and not Python
+                if not (is_python or is_js):
+                    raise ValueError("Server script must be a .py or .js file")
+
+                # the command to execute
+                command = "python" if is_python else "node"
+                server_params = StdioServerParameters(
+                    command = command,
+                    args = [serverScriptPath],
+                    env = None
+                )
+
+                # open a connection to the MCP server.
+                stdio_transport = await self.exit_stack.enter_async_context(stdio_client(server_params))
+                self.stdio, self.write = stdio_transport
+                self.session = await self.exit_stack.enter_async_context(ClientSession(self.stdio, self.write))
+
+                # start session.
+                await self.session.initialize()
+                list_error: bool = False
+  
+                try:
+                    # load all tools.
+                    toolsResult = await self.session.list_tools()
+                    if toolsResult is not None:
+                        if toolsResult.tools is not None:
+                            for tool in toolsResult.tools:
+                                # create the tool model.
+                                self.tools.append(McpTool(
+                                    tool.name,
+                                    tool.name,
+                                    tool.description,
+                                    tool.inputSchema
+                                ))
+                except Exception as etools:
+                    list_error: bool = True
+
+                try:
+                    # load all prompts.
+                    promptsResult = await self.session.list_prompts()
+                    if promptsResult is not None:
+                        if promptsResult.prompts is not None:
+                            for prompt in promptsResult.prompts:
+                                # create the prompt model.
+                                self.prompts.append(McpPrompt(
+                                    prompt.name,
+                                    prompt.name,
+                                    prompt.description,
+                                    prompt.arguments
+                                ))
+                except Exception as eprompts:
+                    list_error: bool = True
+
+                try:
+                    # load all resources.
+                    resourcesResult = await self.session.list_resources()
+                    if resourcesResult is not None:
+                        if resourcesResult.resources is not None:
+                            for resource in resourcesResult.resources:
+                                # create the resource model.
+                                self.resources.append(McpResource(
+                                    resource.name,
+                                    resource.name,
+                                    resource.description,
+                                    resource.uri,
+                                    resource.mimeType
+                                ))
+                except Exception as eresources:
+                    list_error: bool = True
+
+                # client connected.
+                self.open = True
+
+            except Exception as e:
+                self.open = False
+                raise  # Re-throws the same exception
+
+    async def openConnectionHttp(self, serverUrl: str):
+        """
+        connect to the MCP server.
+        start receiving messages on streamable HTTP.
+        For remote servers, set up a Streamable HTTP transport that handles
+        both client requests and server-to-client notifications.
+
+        Args:
+            serverUrl: the server URL path.
+        """
+
+        # if not open.
+        if not self.open:
+            try:
+                # Connect to a streamable HTTP server
+                async with streamablehttp_client(serverUrl) as (
+                    read_stream,
+                    write_stream,
+                    _,
+                ):
+                    # Create a session using the client streams
+                    async with ClientSession(read_stream, write_stream) as session:
+
+                        # assign the client session.
+                        self.httpSession = session
+
+                        # Initialize the connection
+                        await self.httpSession.initialize()
+                        list_error: bool = False
+                
+                        try:
+                            # load all tools.
+                            toolsResult = await self.httpSession.list_tools()
+                            if toolsResult is not None:
+                                if toolsResult.tools is not None:
+                                    for tool in toolsResult.tools:
+                                        # create the tool model.
+                                        self.tools.append(McpTool(
+                                            tool.name,
+                                            tool.name,
+                                            tool.description,
+                                            tool.inputSchema
+                                        ))
+                        except Exception as etools:
+                            list_error: bool = True
+
+                        try:
+                            # load all prompts.
+                            promptsResult = await self.httpSession.list_prompts()
+                            if promptsResult is not None:
+                                if promptsResult.prompts is not None:
+                                    for prompt in promptsResult.prompts:
+                                        # create the prompt model.
+                                        self.prompts.append(McpPrompt(
+                                            prompt.name,
+                                            prompt.name,
+                                            prompt.description,
+                                            prompt.arguments
+                                        ))
+                        except Exception as eprompts:
+                            list_error: bool = True
+
+                        try:
+                            # load all resources.
+                            resourcesResult = await self.httpSession.list_resources()
+                            if resourcesResult is not None:
+                                if resourcesResult.resources is not None:
+                                    for resource in resourcesResult.resources:
+                                        # create the resource model.
+                                        self.resources.append(McpResource(
+                                            resource.name,
+                                            resource.name,
+                                            resource.description,
+                                            resource.uri,
+                                            resource.mimeType
+                                        ))
+                        except Exception as eresources:
+                            list_error: bool = True
+
+                        # client connected.
+                        self.open = True
+
+            except Exception as e:
+                self.open = False
+                raise  # Re-throws the same exception
---
+++ ai/mcp/python/McpServerBase.py
@@ -0,0 +1,283 @@
+from contextlib import asynccontextmanager
+from collections.abc import AsyncIterator
+from typing import Optional, Any, List, Union
+
+from mcp.server.fastmcp import FastMCP
+from mcp.server.fastmcp.resources.base import Resource
+from mcp.server.fastmcp.prompts.base import Prompt
+
+# Model context protocol tool.
+class McpTool:
+    """
+    Model context protocol tool.
+    """
+    def __init__(self,
+                 name: str,
+                 title: str,
+                 description: str,
+                 inputSchema: Any):
+        self.name = name
+        self.title = title
+        self.description = description
+        self.inputSchema = inputSchema
+
+# Model context protocol prompt.
+class McpPrompt:
+    """
+    Model context protocol prompt.
+    """
+    def __init__(self,
+                 name: str,
+                 title: str,
+                 description: str,
+                 arguments: Any):
+        self.name = name
+        self.title = title
+        self.description = description
+        self.arguments = arguments
+
+# Model context protocol resource.
+class McpResource:
+    """
+    Model context protocol resource.
+    """
+    def __init__(self,
+                 name: str,
+                 title: str,
+                 description: str,
+                 uri: str,
+                 mimeType: str):
+        self.name = name
+        self.title = title
+        self.description = description
+        self.uri = uri
+        self.mimeType = mimeType
+
+# Model context protocol server base.
+class McpServerBase:
+    """
+    Model context protocol server base.
+    """
+    def __init__(self,
+                 name: str,
+                 version: str,
+                 instructions: str,
+                 capabilities: Any):
+        """
+        Args:
+            name:    server name
+            version:    server version
+            instructions:    server instructions
+            capabilities:    server capabilities
+
+        Example:
+            name: "weather",
+            version: "1.0.0",
+            instructions: "Use this server for....",
+            capabilities: {
+                resources: {},
+                tools: {},
+            }
+        """
+        self.open = False
+
+        self.name = name
+        self.version = version
+        self.instructions = instructions
+        self.capabilities = capabilities
+
+        # Create an MCP server
+        self.mcp: FastMCP = FastMCP(name, instructions=instructions, stateless_http=True)
+
+    def hasStarted(self) -> bool:
+        """
+        has to MCP server started.
+
+        Return:
+            true if started; else false.
+        """
+        return self.open
+
+    def getMcpServer(self) -> FastMCP:
+        """
+        get the MCP server, used to register Tools, Resource, Prompts.
+
+        Return:
+            the MCP server.
+        """
+        return self.mcp
+
+    def registerTool(self, 
+                     name: str, 
+                     callback: Any,
+                     description: str | None = None,
+                     annotations: Any | None = None) -> bool:
+        """
+        registers a tool with a config object and callback.
+
+        Args:
+            name:  the name of the tool
+            callback:   the callback function.
+            description:    the tool description
+            annotations:    additional tool information
+
+        Return:
+            true if tool is registered; else false.
+
+        Example:
+            "add",
+            lambda a, b: ({
+                content: [{ type: "text", text: str(a + b) }]
+            }),
+            "Add two numbers",
+            annotations: {
+                title: str | None = None  #A human-readable title for the tool.
+            }
+        """
+        result: bool = False
+        try:
+            self.mcp.add_tool(callback, name, description, annotations)
+            result = True
+        except Exception as e:
+            raise
+
+        return result
+
+    def registerResource(self, 
+                     name: str, 
+                     uri: str,
+                     callback: Any,
+                     description: str | None = None,
+                     mimeType: Any | None = None) -> bool:
+        """
+        registers a resource with a config object and callback.
+
+        Args:
+            name:  the name of the resource
+            uri:    the URI
+            callback:   the callback function.
+            description:    the resource description
+            mimeType:    the mime type
+
+        Return:
+            true if resource is registered; else false.
+
+        Example:
+            "config",
+            "config://app",
+            lambda uri: ({
+                contents: [{
+                    uri: uri.href,
+                    text: "App configuration here"
+                }]
+            }),
+            "Application configuration data",
+            "text/plain"
+        """
+        result: bool = False
+        try:
+            self.mcp.add_resource(Resource(
+                callback,
+                uri,
+                name,
+                description,
+                mimeType
+            ))
+            result = True
+        except Exception as e:
+            raise
+
+        return result
+
+    def registerPrompt(self, 
+                     name: str, 
+                     callback: Any,
+                     description: str | None = None,
+                     argsSchema: Any | None = None) -> bool:
+        """
+        registers a prompt with a config object and callback.
+
+        Args:
+            name:  the name of the prompt
+            callback:   the callback function.
+            description:    the prompt description
+            argsSchema:    args schema
+
+        Return:
+            true if prompt is registered; else false.
+
+        Example:
+            "review-code",
+            lambda code: ({
+                messages: [{
+                    role: "user",
+                    content: {
+                        type: "text",
+                        text: `Please review this code:${code}`
+                    }
+                }]
+            }),
+            "Review code for best practices and potential issues",
+            { code: z.string() }
+        """
+        result: bool = False
+        try:
+            self.mcp.add_prompt(Prompt(
+                callback,
+                name,
+                description,
+                argsSchema
+            ))
+            result = True
+        except Exception as e:
+            raise
+
+        return result
+
+    def stopServer(self):
+        """
+        stop receiving messages on stdin and sending messages on stdout
+        """
+        # if open.
+        if self.open:
+            self.mcp = None
+            self.open = False
+
+    def startServerStdio(self):
+        """
+        start receiving messages on stdin and sending messages on stdout.
+        For command-line tools and direct integrations.
+        """
+        # if not open.
+        if not self.open:
+            try:
+                # ... set up server resources, tools, and prompts ...
+                # before starting server.
+                self.mcp.run(transport='stdio')
+
+                # connection open.
+                self.open = True
+
+            except Exception as e:
+                self.open = False
+                raise  # Re-throws the same exception
+
+    def startServerHttp(self):
+        """
+        start receiving messages on streamable HTTP.
+        For remote servers, set up a Streamable HTTP transport that handles
+        both client requests and server-to-client notifications.
+        """
+        # if not open.
+        if not self.open:
+            try:
+                # ... set up server resources, tools, and prompts ...
+                # before starting server.
+                self.mcp.run(transport="streamable-http")
+
+                # connection open.
+                self.open = True
+
+            except Exception as e:
+                self.open = False
+                raise  # Re-throws the same exception
